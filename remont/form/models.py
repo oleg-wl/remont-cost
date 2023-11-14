20ead84cb29f40a362b79511a12ee60cad45750a
oleg_vl <@nitro-pc>
2023-11-14 22:58:44 +0300

    models

---
+++ remont/form/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Types(models.Model):
+    type_name = models.CharField(max_length=200)
+
+class Purchases(models.Model):
+    day = models.DateTimeField()
+    amount = models.FloatField()
+    types = models.ForeignKey(Types, on_delete=models.CASCADE)
+    info = models.CharField(max_length=200)
+ 
+
